776529f9368e48cf0b9c231496612af0718dd592
Chpcunningham <chpcunningham@gmail.com>
2025-11-22 21:18:27 -0500

    Fix bullet damage application

--- Source/DeadPixels/Private/Bullet.cpp
+++ Source/DeadPixels/Private/Bullet.cpp
@@ -1,7 +1,6 @@
 #include "Bullet.h"
 
 #include "Characters/Enemies/EnemyBase.h"
-#include "Kismet/GameplayStatics.h"
 
 ABullet::ABullet()
 {
@@ -50,8 +49,6 @@ void ABullet::OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherA
 	if (AEnemyBase* Enemy = Cast<AEnemyBase>(OtherActor))
 	{
 		GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, FString(TEXT("Hit an enemy")));
-		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-		UGameplayStatics::ApplyDamage(Enemy,1.f, PlayerController,PlayerController, UDamageType::StaticClass() );
 	}
 }
 
